c72e4b0c5d9dee99adf0bbf79b43d33a8f49afe4
Dinh Phi Long Nguyen <dinhplnguyen@gmail.com>
2023-01-21 14:23:03 -0800

    create template

---
+++ nwHack2023/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Sure what do I do from here. Diinh phi long</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
