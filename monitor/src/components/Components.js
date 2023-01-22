8552bd129a384648b76ff07d98d8661be3d78467
iguanaonastick <117231715+iguanaonastick@users.noreply.github.com>
2023-01-22 08:24:11 -0800

    fixed missing import statement

--- monitor/src/components/Components.js
+++ monitor/src/components/Components.js
@@ -5,7 +5,8 @@ import Register from "./register/Register";
 import User from "./user/User";
 import Navbar from "./navbar/Navbar";
 import Error from "./404/404";
+import Thirsty from "thirsty/Thirsty";
 
 import Login2 from "./login/Login2"
 
-export { About, Landing, Login, Register, User, Navbar, Login2 };
+export { About, Landing, Login, Register, User, Navbar, Login2, Thirsty };
