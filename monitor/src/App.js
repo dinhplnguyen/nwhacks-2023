5df7932172c41d796a58adbe91cccd18a8679e09
Dinh Phi Long Nguyen <dinhplnguyen@gmail.com>
2023-01-22 04:44:29 -0800

    error file

--- monitor/src/App.js
+++ monitor/src/App.js
@@ -4,7 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 // import Login from "./components/login/Login";
 
+<<<<<<< Updated upstream
 import { Landing, About, Login, Register, Login2 } from './components/Components';
+=======
+import { Landing, About, Login, Register, Error } from './components/Components';
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -14,8 +18,14 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/about" element={<About />} />
+<<<<<<< Updated upstream
           <Route exact path="/login" element={<Login2 />} />
           <Route exact path="/register" element={<Register />} />
+=======
+          <Route exact path="/Login" element={<Login />} />
+          <Route exact path="/Register" element={<Register />} />
+          <Route path="*" element={<Error />} />
+>>>>>>> Stashed changes
         </Routes>
       </BrowserRouter>
     </div>
--- monitor/src/components/Components.js
+++ monitor/src/components/Components.js
@@ -4,7 +4,12 @@ import Login from "./login/Login";
 import Register from "./register/Register";
 import User from "./user/User";
 import Navbar from "./navbar/Navbar";
+import Error from "./404/404";
 
 import Login2 from "./login/Login2"
 
+<<<<<<< Updated upstream
 export { About, Landing, Login, Register, User, Navbar, Login2 };
+=======
+export { About, Landing, Login, Register, User, Navbar, Error };
+>>>>>>> Stashed changes
